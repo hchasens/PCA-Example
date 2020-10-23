f4b5efbfccbec86169b60b2428a8ede6eab39134
Hunter Chasens <hunter.chasens18@ncf.edu>
2020-10-23 06:47:37 -0400

    pca by percent added

--- pca.py
+++ pca.py
@@ -12,31 +12,34 @@ __version__ = "0.1"
 __email__ = "hunter.chasens18@ncf.edu"
 
 
-def pca(data, d=2, percent=0):
-    """[performs PCA, dimensions xor percent may be used. If both are used then percent will override dimensions.]
-
-    Args:
-        data ([nparray]): [data]
-        d (int, optional): [description]. Defaults to 2.
-        percent (int, optional): [description]. Defaults to 0.
-
-    Returns:
-        [type]: [description]
-    """
-
+def pcaPercent(data, percent=80):
+    assert(percent <= 100 and percent >= 0)
     (norm, mean, std) = Utils.z_score(data, False)      #1. Normalize by Z-Score
     (pc, eig) = Utils.getPC(norm)                #2. Calculate Principal Components
     y = norm @ pc                                #3. Rotate onto Principal Components
+    pc_percent = (eig/np.sum(eig))*100               # Here we find the percentage of each Principal Component
+    d = 0
+    sum = 0
+    for x in pc_percent:
+        sum += x
+        d+=1
+        if sum > percent:
+            break
     y_proj = y[:,0:d]                            #4. Project onto the d-dimensional subspace defined by the first d principal component
     data_rec = (y_proj @ pc[:,0:d].T)*std + mean #5. Reconstruct
+    return data_rec
 
-    #plt.plot(data[:,0], data[:,1], 'or')
-    #plt.plot(data_rec[:,0], data_rec[:,1], 'o')
+def pca(data, d=2):
+    (norm, mean, std) = Utils.z_score(data, False)      #1. Normalize by Z-Score
+    pc = Utils.getPC(norm)                #2. Calculate Principal Components
+    y = norm @ pc                                #3. Rotate onto Principal Components
+    y_proj = y[:,0:d]                            #4. Project onto the d-dimensional subspace defined by the first d principal component
+    data_rec = (y_proj @ pc[:,0:d].T)*std + mean #5. Reconstruct
     return data_rec
 
 def TDExample(data):
-    #plt.imshow(data[0,:,:])
-    plt.imshow(pca(data[0,:,:]))
+    #plt.imshow(data[9,:,:])
+    plt.imshow(pcaPercent(data[8,:,:]))
 
 if __name__=="__main__":
     #iris = Utils.parse("./data/iris.data")
